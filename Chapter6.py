859d34d4de45311a8a40c8ce6cfd50b15af0596a
NeulboGom <jinhee930409@inha.edu>
2023-01-16 14:18:06 +0900

    Chapter6 for statement

--- Chapter6.py
+++ Chapter6.py
@@ -1,19 +1,16 @@
 #Chapter6 반복문
 
-'''dan=int(input("Dan:"))
-i=1
-while i<10:
-    print("{0}*{1}={2}".format(dan,i,dan*i))
-    i+=1
-'''
+#for statement 구구단
+
+dan=int(input("Dan is: "))
 
 while True:
-    dan=int(input("Dan:"))
-    if 1<dan<10:
-        i=1
-        while i<10:
-            print("{0}*{1}={2}".format(dan,i,dan*i))
-            i+=1
+    if dan==0:
+        break
+    elif 1<dan<10:
+        for i in range(1,10):
+            answer=dan*i
+            print(f"{dan}*{i}={answer}")
         break
     else:
-        print("2에서 9사이의 숫자를 입력하세요")
+        print("2에서 9 사이의 숫자를 입력하세요.")
